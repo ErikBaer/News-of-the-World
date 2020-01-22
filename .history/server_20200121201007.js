80a77c8bb9b5f3cf2ae9554a97df72644f7f9d06
Erik Baer <erik.baer@yahoo.de>
2020-01-22 17:31:57 +0100

    erase old code and variables

---
+++ .history/server_20200121201007.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const 
+    express = require('express'),
+    expressHandlebars = require('express-handlebars'),
+    port = process.env.port;
+
+
+
+const server = express();
+
+server.get('/home', (req, res) => {
+    res.render('home', {
+        title: 'News',
+        heading: 'Welcome to your new News Dashboard',
+        articles: articles,
+        homeActive: true
+    })
+})
+
+server.get('/settings', (req, res) => {
+    res.render('settings', {
+        title: 'Settings',
+        heading: 'Welcome to your new Settings',
+        settingsActive: true
+    })
+})
+
+server.set('viewDir', 'views');
+
+server.engine('html', expressHandlebars({
+    extname: 'html',
+    partialsDir: 'partials/'
+}));
+
+server.set('view engine', 'html');
+
+server.use(express.static('public'));
+
+server.listen(port, () => {
+    console.log('Server listening on port: ' + port);
+});
