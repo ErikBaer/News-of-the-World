fc89aac9f815949cb708da5be7b746e00ec32184
Erik Baer <erik.baer@yahoo.de>
2020-01-23 22:29:57 +0100

    delete isLoggedIn variable for better solution

---
+++ .history/controllers/newsController_20200123222753.js
@@ -0,0 +1,30 @@
+const 
+    newsService = require('../services/newsService.js');
+
+
+const renderHome = (req, res) => {
+    let articles = [],
+        message = '';
+
+    newsService.getNews()
+        .then(response => {
+            articles = response.articles
+        })
+        .catch(err => {
+            message= 'Error retrieving articles from newsapi.org';  
+        })
+        .then(response => {
+            res.render('home', {
+                title: 'News',
+                heading: 'Welcome to your new News Dashboard',
+                homeActive: true,
+                articles,
+                message,
+                isLoggedIn: req.session.isLoggedIn
+            });
+        })
+        }
+
+ module.exports = {
+     renderHome
+ }    
