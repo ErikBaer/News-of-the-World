34072b1cb964bb41d7c39eb2d085fb99777cd2dd
Erik Baer <erik.baer@yahoo.de>
2020-01-23 11:04:05 +0100

    move render functions(controller) to routes/index.js

--- .history/routes/index_20200123110158.js
+++ .history/routes/index_20200123110158.js
@@ -25,29 +25,6 @@ const renderHome = (req, res) => {
         })
         }
 
-const renderSettings = (req, res) => {
-    const settings = readSettings() //import json
-    res.render('settings', {
-        title: 'Settings',
-        heading: 'Welcome to your new Settings',
-        settingsActive: true,
-        newsApiKey: settings['news-api-key'] || '',
-        newsApiCategories: newsapi.getCategories().map(categoryName => {
-            return {
-                value: categoryName,
-                label: categoryName,
-                selected:categoryName === settings['news-api-category'] //create object from category-names
-            }
-        })
-    })
-}; // Render Templates when called
-        
-function receiveSettings (req, res) {   
-    fs.writeFileSync('settings.json', JSON.stringify(req.body));
-    renderSettings(req, res); // accepts the request.body, creates a JSON and saves it 
-       
-       }
-
 //home
 server.get('/home', renderHome); // Configure ->get rendered Templates from multiple routes
 server.get('/', renderHome)
