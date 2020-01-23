2813a8b5d6e7c6f8c5f3bf8b558b2f799c6f2758
Erik Baer <erik.baer@yahoo.de>
2020-01-23 11:51:44 +0100

    clean up

---
+++ .history/server_20200123115133.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const 
+    express = require('express'),
+    expressHandlebars = require('express-handlebars'),
+    bodyParser = require('body-parser'),
+    routing = require('./routes'),
+    port = process.env.port; // Get port from .env (Environment Variables)
+
+
+const server = express(); // Initialize server
+
+server.engine('html', expressHandlebars({
+     extname: 'html',
+    partialsDir: 'partials/'
+})); // Define Template engine
+
+server.set('view engine', 'html'); // Set Template engine
+
+server.use(bodyParser.urlencoded({
+    extended: false
+})) // middleware encoding the body of the request to request.body for easy access
+
+server.use(express.static('public')); // Serve style.css directly from public
+
+server.use('/', routing);
+
+server.set('viewDir', 'views'); // Set directory for Templates
+
+server.listen(port, () => {
+    console.log('Server listening on port: ' + port);
+}); // Activate server on defined port
+
+
+
