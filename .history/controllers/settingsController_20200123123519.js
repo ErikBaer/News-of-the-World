4738b9fa178649d06c99bc12a7390dfa4492a530
Erik Baer <erik.baer@yahoo.de>
2020-01-23 12:37:27 +0100

    create and implement newsService

---
+++ .history/controllers/settingsController_20200123123519.js
@@ -0,0 +1,32 @@
+const
+    newsapi = require('newsapi-wrapper'),
+    settingsService = require('../services/settingsService.js');
+
+
+function receiveSettings (req, res) {   
+    settingsService.writeSettings(req.body);
+    renderSettings(req, res); // accepts the request.body, creates a JSON and saves it 
+        
+        }
+
+const renderSettings = (req, res) => {
+    const settings = settingsService.readSettings() //import json
+    res.render('settings', {
+        title: 'Settings',
+        heading: 'Welcome to your new Settings',
+        settingsActive: true,
+        newsApiKey: settings['news-api-key'] || '',
+        newsApiCategories: newsapi.getCategories().map(categoryName => {
+            return {
+                value: categoryName,
+                label: categoryName,
+                selected:categoryName === settings['news-api-category'] //create object from category-names
+            }
+        })
+    })
+}; // Render Templates when called
+
+module.exports = {
+    renderSettings,
+    receiveSettings
+}
